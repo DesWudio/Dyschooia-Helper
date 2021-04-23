4c9a0976393878c86ad05cec7f794e6d36d2a078
DesWudio <46255483+DesWudio@users.noreply.github.com>
2021-04-23 11:17:06 +0800

    Update eat.py

--- eat.py
+++ eat.py
@@ -7,13 +7,12 @@ import time
 # 给出可选内容，手动权重
 Arr = ['麻辣烫', '麻辣拌', '鸡公煲', '渤海', '打卤面', '辣子鸡', '羊汤',
        '二小', '食堂', '食堂', '食堂', '食堂']
-length = len(Arr)
 # 将结果保存到eat.txt
 f = "eat.txt" 
 
 # 按钮事件函数
 def b1():
-    text = Arr[random.randint(0, length-1)]  # 得出随机结果
+    text = Arr[random.randint(0, len(Arr)-1)]  # 得出随机结果
     with open(f, "a") as file:
         file.write(str(time.asctime(time.localtime(time.time()
                     ))) + " " + text + "\n")  # 写入txt格式
